d2f0114e056b3d15cd0bed2f2d22f6e4729c86b3
Faathir81 <4522210033@univpancasila.ac.id>
2024-10-13 08:39:21 +0700

    Pembaruan folder belajar-laravel

--- Pertemuan 6/belajar-laravel/routes/web.php
+++ Pertemuan 6/belajar-laravel/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BukuTamuController;
+use App\Models\BukuTamu;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [BukuTamuController::class, 'index'])->name('home');
+Route::get('/bukutamu', [BukuTamuController::class, 'create'])->name('bukutamu.create');
+Route::post('/bukutamu', [BukuTamuController::class, 'store'])->name('bukutamu.store');
+Route::get('/bukutamu/komentar', [BukuTamuController::class, 'show'])->name('bukutamu.show');
